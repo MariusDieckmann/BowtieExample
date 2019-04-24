eafe03399145ec0abb6b2c8d71bdf98132018453
Marius Dieckmann <mariusdieckmann90@gmail.com>
2019-04-24 12:40:15 +0200

    wrapped env var name

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bowtie2 -x /mnt/workflow_data/raw_data/indexFileName/${raw_data.indexFileName} -1 /mnt/workflow_data/raw_data/input/*_1.fq -2 /mnt/workflow_data/raw_data/input/*_2.fq -S /mnt/workflow_data/results/foo.sam
+bowtie2 -x /mnt/workflow_data/raw_data/indexFileName/$raw_data_indexFileName -1 /mnt/workflow_data/raw_data/input/*_1.fq -2 /mnt/workflow_data/raw_data/input/*_2.fq -S /mnt/workflow_data/results/foo.sam
